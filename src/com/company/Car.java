e1b335ac736ba1d9b007434db8cdf5e0c9819eeb
Sarthak Satish <sarsatis@publicisgroupe.net>
2021-06-26 22:40:10 +0530

    phase 3 devops

---
+++ src/com/company/Car.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Car extends Vehicle{
+
+    public void drive(){
+        System.out.println("Im driving");
+    }
+    public void park(){
+        System.out.println("Im parking");
+    }
+}
---
+++ src/com/company/Vehicle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public abstract class Vehicle {
+    int noOfWheels;
+    String colour;
+    String brand;
+
+    abstract void drive();
+
+
+    abstract void park();
+
+}
